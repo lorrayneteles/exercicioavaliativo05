69f98d343f15ce86eb9e1188c7181c473b33982d
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 21:08:57 -0300

    Add files via upload

---
+++ exercicioavaliativo05.py
@@ -0,0 +1,7 @@
+# Peça ao usuario dois numeros e mostre o resto da divisão deles
+n1 = int(input("Digite o primeiro número:\n"))
+n2 = int(input("Digite o segundo número:\n"))
+
+divisao_resto = ( n1%n2)
+
+print(" O resto da divisão de dois números é:\n", divisao_resto)
